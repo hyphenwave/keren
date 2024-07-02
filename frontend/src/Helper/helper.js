0f8eff4f46f92f0acb4e0f9149500b0cf4ee9959
“hyphenwave” <hyphenwave100@gmail.com>
2024-07-02 09:06:47 +0100

    Checkboxes Fix

--- frontend/src/Helper/helper.js
+++ frontend/src/Helper/helper.js
@@ -1,6 +1,6 @@
 export const TokenAddress = process.env.REACT_APP_USE_TESTNET === 'true'
   ? "0xcA4eea66c3595fAA923e9ec21b738b38c5c62Ea4"  // Testnet address
-  : "0x2B2Eb0781aBe2d16c147C9aC4F04EB327dbd2fCD"; // Mainnet address (replace with actual address)
+  : "0xf9bd1DCaFE10b66fFc4DCb7D4c003DAeb869B044"; // Mainnet address (replace with actual address)
 
 export const metadata = {
   name: "Complain on Chain",
